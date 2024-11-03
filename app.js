6bf068c592164a6ad398372793c8968beed1235e
SunnyKumar-code <sk202542@gmail.com>
2024-11-03 14:29:41 +0530

    add show button

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 let url = "https://openapi.programming-hero.com/api/phones?search=";
 let search = document.querySelector("#btn");
+let showAllBtn = document.querySelector("#showAllBtn"); 
 let phoneBox = document.querySelector("#phoneList");
 let phonename = document.querySelector("#searchInput");
 
-async function phoneSearch() {
+async function phoneSearch(query) {
   phoneBox.innerHTML = "";
-  let res = await fetch(url + phonename.value);
+  let res = await fetch(url + query);
   let data = await res.json();
   let phonearr = data.data;
 
-  for (phone of phonearr) {
+ 
+  let phonesToShow = phonearr.slice(0, 6);
+  renderPhones(phonesToShow);
+
+ 
+  if (phonearr.length > 6) {
+    showAllBtn.style.display = 'block';
+    showAllBtn.onclick = () => {
+      renderPhones(phonearr);
+      showAllBtn.style.display = 'none'; 
+    };
+  } else {
+    showAllBtn.style.display = 'none';
+  }
+}
+
+function renderPhones(phones) {
+  phoneBox.innerHTML = "";
+  for (let phone of phones) {
     let box = document.createElement("div");
     box.className = "phone-item";
     box.innerHTML = `
@@ -24,29 +43,22 @@ async function phoneSearch() {
     showBtn.addEventListener("click", () => {
       document.getElementById('popup-img').src = phone.image;
       document.getElementById('popup-title').innerText = phone.phone_name;
-      document.getElementById('popup-description').innerText = "There are many variations of passages of available, but the majority have suffered"; 
+      document.getElementById('popup-description').innerText = "There are many variations of passages of available, but the majority have suffered";
       document.getElementById('popup').style.display = 'flex';
     });
   }
 }
 
-let showBtn = document.querySelectorAll('#show-btn');
-for(phone of showBtn){
-    phone.addEventListener('click', () => {
-      document.getElementById('popup-description').innerText = "There are many variations of passages of available, but the majority have suffered"; 
-      document.getElementById('popup').style.display = 'flex';
-    });
-  
-}
-
-
 document.querySelector('.close-btn').addEventListener('click', () => {
   document.getElementById('popup').style.display = 'none';
 });
 
-search.addEventListener("click", phoneSearch);
+search.addEventListener("click", () => {
+  phoneSearch(phonename.value);
+});
+
 phonename.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    phoneSearch();
+    phoneSearch(phonename.value);
   }
 });
